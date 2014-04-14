4452fb123bf7ea2e08a582ad569b9a448de8d7a3
Andor Goetzendorff <goetzend@cs.tum.edu>
2014-04-14 13:02:53 +0200

    change default language to markdown

--- db/migrate/002_add_markup_language_to_gollum_wikis.rb
+++ db/migrate/002_add_markup_language_to_gollum_wikis.rb
@@ -1,6 +1,6 @@
 class AddMarkupLanguageToGollumWikis < ActiveRecord::Migration
   def self.up
-    add_column :gollum_wikis, :markup_language, :string, :default => 'textile'
+    add_column :gollum_wikis, :markup_language, :string, :default => 'markdown'
   end
 
   def self.down
