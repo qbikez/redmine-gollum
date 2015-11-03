18d8b89ed09c6d5ca67cac1ca4ba3499823b67ec
Rob Jentzema <rob@sevenmatches.com>
2015-11-03 20:03:35 +0000

    fix for rails 4, first no longer takes conditional arguments this way

--- lib/gollum_project_model_patch.rb
+++ lib/gollum_project_model_patch.rb
@@ -8,7 +8,7 @@ module GollumProjectModelPatch
   module InstanceMethods
     def gollum_wiki
       raise "Need project id." unless self.id
-      return GollumWiki.first(:conditions => ["project_id = ?", self.id]) ||
+      return GollumWiki.where("project_id = ?", self.id) ||
             GollumWiki.new( :project_id => self.id,
                             :git_path => Setting.plugin_redmine_gollum[:gollum_base_path].to_s + "/#{self.identifier}.wiki.git".to_s)
     end
